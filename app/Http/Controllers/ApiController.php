d610035279adcf2428fc202461da40f597392cf3
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-09-30 21:22:07 +0300

    Backend <> using regex to copy letters from a string to another string

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -8,8 +8,10 @@ class ApiController extends Controller
 {
     function sortString() {
         $string = "eA2a1E";
-        // copying the numbers inside $string to another string
+        // copying the numbers inside $string to another string using regex (found searching on google)
         $outputString = preg_replace('/[^0-9]/', '', $string);
-        return $outputString;
+        // copying the letters inside $string to another string using regex (found searching on google)
+        $string = preg_replace("/[^a-zA-Z]+/", "", $string);
+        return $string;
     }
 }
