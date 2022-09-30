a06366c93dd6baf97f53bb336b1a99619175c287
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-09-30 21:57:57 +0300

    Backend <> sorting the letters in the string

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -12,6 +12,17 @@ class ApiController extends Controller
         $int = preg_replace('/[^0-9]/', '', $string);
         // copying the letters inside $string to another string using regex (found searching on google)
         $string = preg_replace("/[^a-zA-Z]+/", "", $string);
-        return $string;
+
+        $int = str_split($int);
+        // $string = str_split($string);
+        $lower_case_string = strtolower($string);
+        $lower_case_string = str_split($lower_case_string);
+        sort($lower_case_string);
+        for($i = 1; $i < count($lower_case_string); $i++) {
+            if ($lower_case_string[$i]==$lower_case_string[$i-1]) {
+                $lower_case_string[$i] = strtoupper($lower_case_string[$i]);
+            }
+        }
+        return $lower_case_string;
     }
 }
