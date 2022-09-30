3fa719be7ae36e6f1828e9aad224e181f7612a94
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-09-30 21:24:30 +0300

    Backend <> updating variable name

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,7 +9,7 @@ class ApiController extends Controller
     function sortString() {
         $string = "eA2a1E";
         // copying the numbers inside $string to another string using regex (found searching on google)
-        $outputString = preg_replace('/[^0-9]/', '', $string);
+        $int = preg_replace('/[^0-9]/', '', $string);
         // copying the letters inside $string to another string using regex (found searching on google)
         $string = preg_replace("/[^a-zA-Z]+/", "", $string);
         return $string;
