27e47b546dc47928247d1457e6438ae01de63e52
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-09-30 21:12:21 +0300

    Backend <> using regex to copy numbers from a string to another string

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,7 +7,9 @@ use Illuminate\Http\Request;
 class ApiController extends Controller
 {
     function sortString() {
-        $string = "7deh5eFjsy4";
-        return $string;
+        $string = "eA2a1E";
+        // copying the numbers inside $string to another string
+        $outputString = preg_replace('/[^0-9]/', '', $string);
+        return $outputString;
     }
 }
